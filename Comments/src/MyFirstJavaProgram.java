78cf9ae20b2c98970f13b0dea197ac3f177e2b84
Ayush Dey <62686696+dey-ayush@users.noreply.github.com>
2022-09-19 16:35:39 +0530

    Add files via upload

---
+++ Comments/src/MyFirstJavaProgram.java
@@ -0,0 +1,18 @@
+
+public class MyFirstJavaProgram {
+	
+	/*This is my First Java Program
+	  It will print 'Hello World' as output
+	  This is an example of multi-line comments.
+	 */
+
+	public static void main(String[] args) {
+		
+		// This is an example of single-line comment.
+		/* This is also a single line comment. */
+		
+		System.out.println("Hello World");
+
+	}
+
+}
