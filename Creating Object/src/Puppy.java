096a7b434260579cb50b018a33a0c8b1440014be
Ayush Dey <62686696+dey-ayush@users.noreply.github.com>
2022-09-21 17:06:54 +0530

    Creating an Object

---
+++ Creating Object/src/Puppy.java
@@ -0,0 +1,14 @@
+
+public class Puppy {
+	public Puppy(String name) {
+		//This constructor has one parameter; name
+		System.out.println("Passed name: "+ name);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Following statement would create object myPuppy
+		Puppy myPuppy= new Puppy("tommy");
+	}
+
+}
