6df467d643e3316f1e2f669d9b83e8a1faed3b1b
Ayush Dey <62686696+dey-ayush@users.noreply.github.com>
2022-09-24 20:57:20 +0530

    Java Constructors example- no arguments

---
+++ No Argument Constructor/src/ConsDemo.java
@@ -0,0 +1,12 @@
+
+public class ConsDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MyClass t1= new MyClass();
+		MyClass t2= new MyClass();
+		System.out.println(t1.num +" "+ t2.num);
+
+	}
+
+}
---
+++ No Argument Constructor/src/MyClass.java
@@ -0,0 +1,8 @@
+
+public class MyClass {
+	int num;
+	MyClass(){
+		num=100;
+	}
+
+}
