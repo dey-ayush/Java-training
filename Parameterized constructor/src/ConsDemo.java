c3833276e2c2e704da772bf493d22ff1974d5002
Ayush Dey <62686696+dey-ayush@users.noreply.github.com>
2022-09-25 19:12:15 +0530

    Java Constructors example- Parameterized

---
+++ Parameterized constructor/src/ConsDemo.java
@@ -0,0 +1,11 @@
+
+public class ConsDemo {
+
+	public static void main(String args[]) {
+		// TODO Auto-generated constructor stub
+		MyClass t1=new MyClass(10);
+		MyClass t2=new MyClass(20);
+		System.out.println(t1.x+" "+t2.x);
+	}
+
+}
---
+++ Parameterized constructor/src/MyClass.java
@@ -0,0 +1,10 @@
+
+class MyClass {
+	int x;
+
+	MyClass(int i) {
+		// TODO Auto-generated constructor stub
+		x=i;
+	}
+
+}
