087f152eeacd843a7f3d1f80813115f4f60afd77
Ayush Dey <62686696+dey-ayush@users.noreply.github.com>
2022-09-30 15:53:57 +0530

    Example of Java Basic Operators

---
+++ Arithmetic Operator/src/Test.java
@@ -0,0 +1,24 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a=10, b=20, c=25, d=25;
+		
+		System.out.println("a+b= "+ (a+b));
+		System.out.println("a-b= "+ (a-b));
+		System.out.println("a*b= "+ (a*b));
+		System.out.println("b/a= "+ (b/a));
+		System.out.println("b%a= "+ (b%a));
+		System.out.println("c%a= "+ (c%a));
+		System.out.println("a++= "+ (a++));
+		System.out.println("a--= "+ (a--));
+		
+		//Check the difference in d++ and ++d:
+		System.out.println("d++ "+ (d++));
+		System.out.println("++d "+ (++d));
+
+	}
+
+}
