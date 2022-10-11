5a9e5e250f46e5f6f8d8ffbb568f6261bc3d7912
Ayush Dey <62686696+dey-ayush@users.noreply.github.com>
2022-10-11 18:13:30 +0530

    Example of Java Loop Control

---
+++ While loop/src/Test.java
@@ -0,0 +1,13 @@
+
+public class Test {
+	public static void main(String args[]) {
+		
+		int x=10;
+		while(x<20) {
+			System.out.print("Value of x: "+x);
+			x++;
+			System.out.print("\n");
+		}
+	}
+
+}
