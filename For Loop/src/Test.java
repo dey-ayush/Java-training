a030b0f33b83bcad6c90d15aa0c91c22e1a567a1
Ayush Dey <62686696+dey-ayush@users.noreply.github.com>
2022-10-12 17:15:15 +0530

    Example of Java Loop Control

---
+++ For Loop/src/Test.java
@@ -0,0 +1,14 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for( int x=10; x<20; x++) {
+			System.out.print("Value of x: "+x);
+			System.out.print("\n");
+		}
+
+	}
+
+}
