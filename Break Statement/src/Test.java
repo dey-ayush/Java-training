b2448fa577b5402155f45519bb20d323cf6f7218
Ayush Dey <62686696+dey-ayush@users.noreply.github.com>
2022-10-14 18:06:14 +0530

    Example of Loop Control Statements

---
+++ Break Statement/src/Test.java
@@ -0,0 +1,18 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int [] numbers= {10, 20, 30, 40, 50};
+		
+		for(int x: numbers) {
+			if (x==30) {
+				break;
+			}
+			System.out.print(x);
+			System.out.print("\n");
+			
+		}
+	}
+}
