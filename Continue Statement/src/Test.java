75d3f60b5908bf4679376e66609237960c474525
Ayush Dey <62686696+dey-ayush@users.noreply.github.com>
2022-10-15 17:04:48 +0530

    Example of Loop Control Statements

---
+++ Continue Statement/src/Test.java
@@ -0,0 +1,20 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int [] nums= {10, 20, 30, 40, 50};
+		
+		for (int x: nums) {
+			if( x==30) {
+				continue;
+			}
+			System.out.print(x);
+			System.out.print("\n");
+			
+		}
+
+	}
+
+}
