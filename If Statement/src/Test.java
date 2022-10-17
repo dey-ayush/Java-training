c90dd3aa8c50b94448c5a0af664abb83b7f01044
Ayush Dey <62686696+dey-ayush@users.noreply.github.com>
2022-10-17 20:09:15 +0530

    Example of Java Decision Making

---
+++ If Statement/src/Test.java
@@ -0,0 +1,14 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int x=10;
+		if (x<20) {
+			System.out.println("This is if statement.");
+		}
+
+	}
+
+}
