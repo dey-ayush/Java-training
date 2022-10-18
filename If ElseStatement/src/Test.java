ddc36b69fb2823cc024cfb97564f4301844b710a
Ayush Dey <62686696+dey-ayush@users.noreply.github.com>
2022-10-18 17:37:32 +0530

    Example of Java Decision Making

---
+++ If ElseStatement/src/Test.java
@@ -0,0 +1,19 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int x=30;
+		
+		if(x<20) {
+			System.out.println("This is if statement");		
+		}
+		
+		else {
+			System.out.println("This is else statement");
+		}
+
+	}
+
+}
