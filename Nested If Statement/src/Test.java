e6519e90a5bb5c593076974d76f76227be4ee7cf
Ayush Dey <62686696+dey-ayush@users.noreply.github.com>
2022-10-19 19:31:33 +0530

    Example of Java Decision Making

---
+++ Nested If Statement/src/Test.java
@@ -0,0 +1,15 @@
+
+public class Test {
+	public static void main(String args[]) {
+		
+		int x=10;
+		int y=20;
+		
+		if(x==10) {
+			if(y==20) {
+				System.out.println("x=10 and y=20");
+			}
+		}
+	}
+
+}
