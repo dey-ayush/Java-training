8f76360b06481c4e0ecb1a3bcab8fa34e25f268d
Ayush Dey <62686696+dey-ayush@users.noreply.github.com>
2022-10-21 20:49:21 +0530

    Example of Java Number Class

---
+++ Number class/src/Test.java
@@ -0,0 +1,13 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Integer x=5;    //boxes int to Integer object
+		x= x+10;        //unboxes Integer to int
+		System.out.println(x);
+
+	}
+
+}
