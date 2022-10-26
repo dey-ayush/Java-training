29b41641a5f64ba22f5b3a48dce55913c5e05463
Ayush Dey <62686696+dey-ayush@users.noreply.github.com>
2022-10-26 20:41:54 +0530

    Example of String Length

---
+++ String Lenght/src/StringDemo.java
@@ -0,0 +1,13 @@
+
+public class StringDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String palindrome= "dot saw i was tod";
+		int len=palindrome.length();
+		System.out.println("String Lenght is: "+len);
+
+	}
+
+}
