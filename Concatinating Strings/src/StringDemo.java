6ee14c2ae3be682b7605c712d1c8dc80245610c6
Ayush Dey <62686696+dey-ayush@users.noreply.github.com>
2022-10-27 21:29:03 +0530

    Example of Concatenating Strings

---
+++ Concatinating Strings/src/StringDemo.java
@@ -0,0 +1,12 @@
+
+public class StringDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String string1= "saw I was";
+		System.out.println("Dot "+ string1+ " toD");
+
+	}
+
+}
