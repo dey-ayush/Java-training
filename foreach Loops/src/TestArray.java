3bb37cb048bd5b2eb3be27c90d97051fa30da35b
Ayush Dey <62686696+dey-ayush@users.noreply.github.com>
2022-10-29 20:48:05 +0530

    Example of foreach Loop

---
+++ foreach Loops/src/TestArray.java
@@ -0,0 +1,16 @@
+
+public class TestArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		double[] myList= {1.9, 2.9, 3.4, 3.5};
+		
+		//Print all array elements
+		for(double element: myList) {
+			System.out.println(element);
+		}
+	}
+
+}
