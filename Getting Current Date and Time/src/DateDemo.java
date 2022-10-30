5a1cbab7981c5a264f3385e7387246ebbd579c74
Ayush Dey <62686696+dey-ayush@users.noreply.github.com>
2022-10-30 20:32:12 +0530

    Example of getting current date & time

---
+++ Getting Current Date and Time/src/DateDemo.java
@@ -0,0 +1,16 @@
+import java.util.Date;
+
+public class DateDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Instantiate a Date object
+		Date date= new Date();
+		
+		//display time and date using toString()
+		System.out.println(date.toString());
+
+	}
+
+}
