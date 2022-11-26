cae5011aa0fc41d8884a5b1a79b038ab4f72ad06
Ayush Dey <62686696+dey-ayush@users.noreply.github.com>
2022-11-26 22:04:04 +0530

    Example of Exceptions

---
+++ Checked Exceptions/src/FileNotFound_Demo.java
@@ -0,0 +1,14 @@
+import java.io.File;
+import java.io.FileReader;
+
+public class FileNotFound_Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		File file= new File("E://file.txt");
+		FileReader fr= new FileReader(file);
+
+	}
+
+}
