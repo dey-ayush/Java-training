58b77a60709fde44749f1d300e5107720f3bc4e2
Ayush Dey <62686696+dey-ayush@users.noreply.github.com>
2022-11-27 22:29:23 +0530

    Example of Exceptions

---
+++ Unchecked Exceptions/src/Unchecked_Demo.java
@@ -0,0 +1,12 @@
+
+public class Unchecked_Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int num[] = {1,2,3,4};
+		System.out.println(num[5]);
+
+	}
+
+}
