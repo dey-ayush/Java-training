76d93cd6268fcc5152debd29c8c4aad0b498df39
Ayush Dey <62686696+dey-ayush@users.noreply.github.com>
2022-12-09 22:05:36 +0530

    Example of Inner Classes

---
+++ Static Nested Class/src/Outer.java
@@ -0,0 +1,17 @@
+
+public class Outer {
+	static class Nested_Demo{
+		public void my_method() {
+			System.out.println("This is my nested class");
+		}
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Outer.Nested_Demo nested= new Outer.Nested_Demo();
+		nested.my_method();
+
+	}
+
+}
