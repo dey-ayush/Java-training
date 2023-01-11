31aaa85982f379c41e922301724dee5dccebd705
Ayush Dey <62686696+dey-ayush@users.noreply.github.com>
2023-01-11 19:58:59 +0530

    Example of Abstraction

---
+++ Abstract Class/src/AbstractDemo.java
@@ -0,0 +1,13 @@
+
+public class AbstractDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Following is illegal and will raise an error
+		Employee e= new Employee("Ram P.", "Banaras", 43);
+		System.out.println("\nCall mailCheck using Employee reference...");
+
+	}
+
+}
